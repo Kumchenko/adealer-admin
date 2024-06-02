8d1a74b21ba71231e47c61bc14db900475afc395
Kyrylo Kumchenko <kirillkumchenko@gmail.com>
2024-06-02 12:44:05 +0300

    feat: captcha

--- src/app/(auth)/login/components/LoginSection/LoginSection.tsx
+++ src/app/(auth)/login/components/LoginSection/LoginSection.tsx
@@ -7,7 +7,7 @@ const LoginSection = () => {
     <Section full={true}>
       <h1 className="text-center text-h2 font-medium sm:text-h1">AppleDealer Managing System</h1>
       <p className="mt-4 text-center text-xl sm:text-2xl">Authorized access only!</p>
-      <Card className="mx-auto mt-8 w-5/6 sm:mt-12 sm:w-80">
+      <Card className="relative mx-auto mt-8 w-5/6 sm:mt-12 sm:w-80">
         <LoginForm />
       </Card>
     </Section>
